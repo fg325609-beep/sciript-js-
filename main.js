044fb83f4fd5747254b6d715f4ae6cf0fe95edf1
Isming <email@example.com>
2026-03-28 11:54:02 +0500

    kjdksjdksdksjdksjdsjdjk

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-let number = prompt("son kiriting");
-// let number = 10;
+// let number = prompt("son kiriting");
+// // let number = 10;
 
 
-if (number / 2 +1 && number < 2 -2 ) {
-    alert("tog'ri son")
+// if (number / 2 +1 && number < 2 -2 ) {
+//     alert("tog'ri son")
     
-} else {
-    alert(" no togri son")
-}
-console.log(number);
+// } else {
+//     alert(" no togri son")
+// }
+// console.log(number);
+
+
+// let a = +prompt("son kiriting");
+
+// if (a >0) {
+//     alert( a +1)
+    
+// } else if (a < 0){
+//     alert(a -2)
+    
+// }
+
+
+
+
