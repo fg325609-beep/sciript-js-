07cea94220aa81bef4faa8fb4f6a5be57d7ba729
Isming <email@example.com>
2026-03-28 11:46:58 +0500

    hdfjdhfjdh

---
+++ main.js
@@ -0,0 +1,11 @@
+let number = prompt("son kiriting");
+// let number = 10;
+
+
+if (number / 2 +1 && number < 2 -2 ) {
+    alert("tog'ri son")
+    
+} else {
+    alert(" no togri son")
+}
+console.log(number);
