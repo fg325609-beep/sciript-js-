fb2f03d7c64666dfb4197911c8db9094a44de2c1
Isming <email@example.com>
2026-03-28 12:02:57 +0500

    jghd

--- main.js
+++ main.js
@@ -23,4 +23,15 @@
 
 
 
+let a = +prompt("a sonini kiriting");
+let b = +prompt("b sonini kiriting");
+let c = +prompt("c sonini kiriting");
 
+
+if ( a > 0 && b > 0 && c > 0 ) {
+    alert(" son musbat")
+    
+} else if (a < 0 && b < 0 && c < 0){
+    alert("son manfiy")
+    
+}
